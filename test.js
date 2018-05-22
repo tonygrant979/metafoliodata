ee92028d701efc6c4f92402f3b5a4f8ae523409a
John Thantranon <jthantranon@gmail.com>
2018-05-22 11:44:01 -0700

    auto!

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
-let go = '',
-    fly = '2w',
-    a = 'r2',
-    kite = 'as';
+// let go = '',
+//     fly = '2w',
+//     a = 'r2',
+//     kite = 'as';
+//
+// let array = [go,fly,a,kite];
+//
+// // console.log(array.toString())
+// let out = '';
+// console.log();
+// let astring = `[go,fly,a,kite];`;
+// out = eval(astring);
+// console.log(out);
 
-let array = [go,fly,a,kite];
+const Axios = require('axios');
 
-// console.log(array.toString())
-let out = '';
-console.log();
-let astring = `[go,fly,a,kite];`;
-out = eval(astring);
-console.log(out);
+const reddit  = Axios.create({baseURL: 'https://gateway.reddit.com/desktopapi/v1/subreddits'});
+reddit.get('/litecoin?rtj=debug&redditWebClient=web2x&app=web2x-client-production&allow_over18=&dist=9&layout=classic&sort=hot').then( data => {
+    console.log(data.data)
+});
+// "https://gateway.reddit.com/desktopapi/v1/subreddits/litecoin?rtj=debug&redditWebClient=web2x&app=web2x-client-production&allow_over18=&dist=9&layout=classic&sort=hot"
