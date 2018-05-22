768b53c93ec9e1ada27cec7471879920b4aee473
John Thantranon <jthantranon@gmail.com>
2018-05-22 11:48:02 -0700

    auto!

--- test.js
+++ test.js
@@ -16,6 +16,17 @@ const Axios = require('axios');
 
 const reddit  = Axios.create({baseURL: 'https://gateway.reddit.com/desktopapi/v1/subreddits'});
 reddit.get('/litecoin?rtj=debug&redditWebClient=web2x&app=web2x-client-production&allow_over18=&dist=9&layout=classic&sort=hot').then( data => {
-    console.log(data.data)
+    console.log();
+    let high = {
+        score: 0
+    };
+    let posts = data.data.posts;
+    Object.keys(posts).forEach(pid =>{
+        let post = posts[pid];
+        if(post.isStickied) return;
+        if(post.score > high.score) high = post;
+    });
+    console.log(high);
+    // console.log(Date.parse(high.created));
 });
 // "https://gateway.reddit.com/desktopapi/v1/subreddits/litecoin?rtj=debug&redditWebClient=web2x&app=web2x-client-production&allow_over18=&dist=9&layout=classic&sort=hot"
