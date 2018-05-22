0e9a79b794e61dcc3645ab221d618ec50b34730e
John Thantranon <jthantranon@gmail.com>
2018-05-22 00:04:03 -0700

    auto!

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 let go = '',
-    fly = '',
-    a = '',
-    kite = '';
+    fly = '2w',
+    a = 'r2',
+    kite = 'as';
 
 let array = [go,fly,a,kite];
 
-console.log(array.toString())
+// console.log(array.toString())
+let out = '';
+console.log();
+eval(`out = [go,fly,a,kite];`)
+console.log(out);
