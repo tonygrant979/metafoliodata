232fa4cff12151404bc6bf37cdb11fca4015e5cd
JFTActual <jthantranon@gmail.com>
2018-07-10 21:40:02 -0700

    Rhode Island Pope Sea Lion

--- swgcoords.js
+++ swgcoords.js
@@ -1,5 +1,13 @@
 let coords = {
     tatooine: {
-        'mos eisley': [3608,-4753]
+        'mos eisley': [3608,-4753],
+        'bestine': [-1294, -3590]
     }
 };
+
+function find(planet,coords){
+    let planetCoords = coords[planet];
+    Object.keys(planetCoords).forEach(starport=>{
+        
+    })
+}
